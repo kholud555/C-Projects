737f695d9733461dc5196457bd1dd32da626401f
KHOLUD AHMED <kholudahmed666@gmail.com>
2024-11-13 19:25:19 +0200

    Add new project: project 3

---
+++ Prodject 3/clsMyStack.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include "clsMyQueue.h"
+using namespace std;
+template <class T>
+
+class  clsMyStack :public   clsMyQueue <T>
+{
+
+
+public:
+
+	void push(T Item)
+	{
+		this->InsertAtFront(Item);
+	}
+
+	T Top()
+	{
+		return this->front();
+
+	}
+
+	T Bottom()
+	{
+		return this->back();
+
+	}
+
+};
+
